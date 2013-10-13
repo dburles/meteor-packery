8e327fc10c5cbb0120b0df08677d8ca42f8cfbc7
David Burles <dburles@gmail.com>
2013-10-13 17:15:10 +1100

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Packery
+
+_Bin-packing layout library_
+
+See http://packery.metafizzy.co for complete docs and demos
---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "Packery - Bin-packing layout library"
+});
+
+Package.on_use(function(api) {
+  api.add_files('packery.pkgd.js', 'client');
+});
